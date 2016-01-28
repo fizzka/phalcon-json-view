ba6c4a91093a700250fcc6c44d167a371d9fac96
Ilya Bulah <ilya.bulah@gmail.com>
2016-01-28 08:20:36 +0300

    add support for pretty

--- src/JsonView.php
+++ src/JsonView.php
@@ -11,12 +11,15 @@ class JsonView extends Plugin {
 	public function afterDispatchLoop(Event $event, Dispatcher $dispatcher) {
 		$data = $dispatcher->getReturnedValue();
 		if (is_array($data)) {
-			$f = new FilterUnicode();
-			$data = $f->filter(json_encode($data));
+			$data = json_encode($data, $this->getJsonOptions($dispatcher->getParam('pretty')));
 		}
 
 		$this->response->setContentType('application/json', 'UTF-8');
 		$this->response->setContent($data);
 		$dispatcher->setReturnedValue($this->response);
+	}
+
+	protected function getJsonOptions($pretty = false) {
+		return JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | ($pretty ? JSON_PRETTY_PRINT : 0);
 	}
 }
