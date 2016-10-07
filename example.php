e21298b1555c0dfd05d9763925c069fad2d58255
Ilya Bulah <bulah@1mps.ru>
2016-10-07 15:12:12 +0300

    add example

---
+++ example.php
@@ -0,0 +1,46 @@
+<?php
+
+use Fizz\Phalcon\JsonController;
+use Fizz\Phalcon\JsonView;
+use Phalcon\Mvc\Dispatcher;
+use Phalcon\Mvc\View;
+
+use Phalcon\DI\FactoryDefault as DI;
+use Phalcon\Mvc\Application as PhalconApp;
+
+include 'vendor/autoload.php';
+
+$di = new DI();
+
+# Step 1
+$di->set('dispatcher', function () use ($di) {
+    $eventsManager = $di->getShared('eventsManager');
+
+    $json = new JsonView();
+
+    $eventsManager->attach('dispatch:afterDispatchLoop', $json);
+
+    $dispatcher = new Dispatcher();
+
+    $dispatcher->setEventsManager($eventsManager);
+    return $dispatcher;
+}, true);
+
+# Step 2
+$di->set('view', function () {
+    $view = new View();
+    $view->disable();
+    return $view;
+}, true);
+
+# Step 3
+class ExampleController extends JsonController
+{
+	public function indexAction()
+	{
+		return ['foo' => 'baz'];
+	}
+}
+
+$app = new PhalconApp($di);
+echo $app->handle('/example/index')->getContent();
